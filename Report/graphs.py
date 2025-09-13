94040d62d6043d06fe432e91ac5e6e9a87dd9ea2
Nikolas <ioanikolas@gmail.com>
2025-09-13 21:32:42 +0300

    Graphs left and abstract

--- Report/graphs.py
+++ Report/graphs.py
@@ -1,3 +1,4 @@
+import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -9,35 +10,27 @@ iterations = np.arange(10000, 140001, 10000)
 iterations = np.append(iterations, 148800)
 # PQ values for three different methods (replace with your data)
 x = [
-    29.215, 34.636, 38.009, 40.211, 43.68, 45.705,
-    46.752, 49.964, 50.461, 52.189, 53.035,
-    53.473, 53.686, 54.69, 55.365
+    27.592, 33.733, 37.147, 39.815, 43.020,
+    44.141, 47.438, 48.528, 52.355, 51.566,
+    52.528, 53.524, 55.573, 55.288, 55.436
 ]
 
 y = [
-    17.495, 21.852, 24.983, 28.493, 30.031, 31.725,
-    33.57, 36.308, 38.035, 38.881, 39.043,
-    40.559, 40.719, 41.802, 42.495
-]
-
-z  = [
-    15.946, 21.153, 23.849, 27.126, 28.461, 29.909,
-    31.5, 31.553, 34.98, 35.932, 35.798,
-    37.162, 37.507, 38.563, 38.686
+    26.641, 33.573, 37.123, 39.929, 43.482,
+    46.132, 47.324, 47.998, 50.388, 52.798,
+    53.952, 51.16, 53.751, 56.172, 55.123
 ]
 
 
 # Plot each method
 plt.figure(figsize=(8,5))
-plt.plot(iterations, x, marker='o', label="MLP4-BN")
-plt.plot(iterations, y, marker='o', label="MLP4-GN")
-plt.plot(iterations, z, marker='o', label="MLP4-BGN")
-
+plt.plot(iterations, x, marker='o', label="VANmb-20000")
+plt.plot(iterations, y, marker='o', label="VANmb-40000")
 
 # Labels and title
 plt.xlabel("Επαναλήψεις")
 plt.ylabel("PQ")
-plt.title("PQ vs Επαναλήψεις του MLP4 για Διαφορετικές Μεθόδους Κανονικοποίησης")
+plt.title("PQ vs Επαναλήψεις του MLP4 για Διαφορετικές Τιμές Επαναλήψεων Προσαρμογής της Θερμοκρασίας")
 
 # Legend and grid
 plt.legend(title="Μέθοδος")
